9db93db3bb21745235c49b437b00eff3e5ca7c0c
Shamsul Majid <66409646+ShmslMjd@users.noreply.github.com>
2021-09-15 12:46:25 +0800

    Add all file

---
+++ Prototype 5/Assets/Script/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; //import TMPRo package so that we can interact with the score
+using UnityEngine.SceneManagement; //to interact and manage scene in the folder
+using UnityEngine.UI; //to interact with the button
+
+public class GameManager : MonoBehaviour
+{
+
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
+    public bool isGameActive;
+    private int score; //variable to store score
+    private float spawnRate = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate); //control the length of time between each spawn
+            int index = Random.Range(0, targets.Count); //target.Count count number of object in the list in this case its 4
+            Instantiate(targets[index]);
+
+        }
+    }
+
+    public void UpdateScore (int ScoreToAdd)
+    {
+        score += ScoreToAdd;
+        scoreText.text = "Score: " + score; //link with score so the score will update later
+    }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //load the starting scene
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        //set the variable first before the function
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0); //display score 0 at start
+
+        titleScreen.gameObject.SetActive(false);
+    }
+}
---
+++ Prototype 5/Assets/Script/Target.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private GameManager gameManager; //variable for game manager
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = 2;
+
+    public ParticleSystem explosionParticle;
+    public int pointValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>(); //find the Game Manager object and find the Game Manager script
+
+        //push target up by random force value
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        //spawn object randomly from below
+        transform.position = RandomSpawnPos();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+
+        if(!gameObject.CompareTag("Bad")) //if the game object than run to the sensor is not bad then game over. Exclamation point will negate the statement
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), -ySpawnPos);
+    }
+}
